0342c502961d3b9982c32a52e5bbb47efbda75e1
aaronworsnop <aaronworsnop@gmail.com>
2023-09-07 20:24:36 +1200

    More robust actions and accessability for GameMaster

--- src/main/java/nz/ac/auckland/se206/controllers/GameMasterActions.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GameMasterActions.java
@@ -2,7 +2,9 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.animation.FadeTransition;
 import javafx.scene.control.TextArea;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class GameMasterActions {
@@ -10,40 +12,66 @@ public class GameMasterActions {
   private TextArea textArea;
   private boolean active;
 
+  public GameMasterActions() {}
+
   public GameMasterActions(ImageView image, TextArea textArea) {
     this.image = image;
-    // Setting opacity to 0;
     this.image.setOpacity(0);
+
     this.textArea = textArea;
+    textArea.setText("");
+    InnerShadow innerShadow = new InnerShadow();
+    innerShadow.setColor(Color.WHITE);
+    innerShadow.setRadius(10.0);
+    textArea.setEffect(innerShadow);
   }
 
   public void isActive(boolean active) {
     this.active = active;
   }
 
-  /** Shows the GameMaster image with a fade-in animation. */
-  public void show() {
+  /** Shows the GameMaster with a fade-in animation. */
+  public void activate() {
+    if (isActive()) {
+      return;
+    }
     setActive();
 
     // Show the GameMaster image with a 1s fade-in animation.
-    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.0), image);
+    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), image);
     fadeTransition.setFromValue(0.0);
     fadeTransition.setToValue(1.0);
     fadeTransition.play();
 
-    // Show the GameMaster glow with a 1s fade-in animation.
+    // Show the GameMaster glow
+    InnerShadow innerShadow = new InnerShadow();
+    innerShadow.setColor(Color.RED);
+    innerShadow.setRadius(10.0);
+    textArea.setEffect(innerShadow);
   }
 
-  public void hide() {
+  /** Hides the GameMaster with a fade-out animation. */
+  public void unactivate() {
+    if (!isActive()) {
+      return;
+    }
     setInactive();
 
     // Hide the GameMaster image with a 1s fade-in animation.
-    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.0), image);
+    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), image);
     fadeTransition.setFromValue(1.0);
     fadeTransition.setToValue(0.0);
     fadeTransition.play();
 
-    // Hide the GameMaster glow with a 1s fade-in animation.
+    // Hide the GameMaster glow
+    InnerShadow innerShadow = new InnerShadow();
+    innerShadow.setColor(Color.WHITE);
+    innerShadow.setRadius(10.0);
+    textArea.setEffect(innerShadow);
+  }
+
+  public boolean isActive() {
+    return this.active;
   }
 
   private void setActive() {
