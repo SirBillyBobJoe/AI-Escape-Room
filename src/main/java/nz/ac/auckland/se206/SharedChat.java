e72f9b59aea67ba0f6997fedb572b4b95261dc53
aaronworsnop <aaronworsnop@gmail.com>
2023-10-15 21:49:13 +1300

    Fix: Game master no longer hallucinates about the player being incorrect

--- src/main/java/nz/ac/auckland/se206/SharedChat.java
+++ src/main/java/nz/ac/auckland/se206/SharedChat.java
@@ -84,7 +84,7 @@ public class SharedChat {
 
     // If the player gives the correct answer to the game master, instead of the computer
     if (msg.contains(GameState.passcodeAnswer)) {
-      msg1 = "Tell the player to enter their correct answer in the computer instead of telling you. ";
+      msg1 = "The player is correct. Tell the player to enter their correct answer in the computer instead of telling you. ";
       textField.clear();
     } else {
     msg = msg.toLowerCase();
